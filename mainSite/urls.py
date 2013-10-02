ec53d1b4f48d18b9cee48b99b63064717ccd493d
amity177 <amity.mio@gmail.com>
2013-10-01 22:05:26 -0700

    new structure and added background color under background img.

--- mainSite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from mainSite import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.landing, name='landing'),
-    url(r'^testing/$', views.testing, name='testing'),
-
-)
--- src/app/js/directives.js
+++
@@ -1,6 +0,0 @@
-angular.module("components", [])
-	.directive("navbar", function() {
-	return {
-		restrict: "E",
-		templateUrl: "/food2cube/src/components/navbar.html"
-	};});
